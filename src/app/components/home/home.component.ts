ae05248b20f9755f4340709134c73978ee76af78
sanketvagadiya <scvagadiya@gmail.com>
2020-02-29 21:56:21 -0500

    dynamic list item added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  listItems=["ॐ नमः शिवाय 1","ॐ नमः शिवाय 2 ","ॐ नमः शिवाय 3"]
   constructor() { }
 
   ngOnInit() {
   }
-
+  listItemClick(index){
+    console.log( "."+index);
+    
+  }
 }
